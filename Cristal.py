c85fad47c694fbcd0f620544a2a0dfe7123d3ef8
blacksoft <blacksoft@int.pl>
2020-02-03 13:30:39 +0100

    Added hello world window

---
+++ Cristal.py
@@ -0,0 +1,30 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+from ui import MainWindow, testDialog
+
+class Main(QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = MainWindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.helloWorld)
+        print("Program loaded ...")
+
+    def helloWorld(self):
+        self.dialog = HelloWorld()
+        self.dialog.show()
+
+
+class HelloWorld(QDialog):
+    def __init__(self):
+        super(HelloWorld, self).__init__()
+        self.ui = testDialog.Ui_Dialog()
+        self.ui.setupUi(self)
+
+
+app = QApplication(sys.argv)
+ui = Main()
+ui.show()
+sys.exit(app.exec_())
